68484d6619302527e18d293eba58f36ea5b9f226
ggarciacyb1989 <ggarciacyb1989@yahoo.com>
2021-06-02 15:15:15 -0500

    A comment was added.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitHub_Test3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
